59c096ca2b0f9a05b93a62b4d86b71df48caf05c
Michael Giambalvo <heathkit@google.com>
2017-07-18 02:06:02 -0700

    Step 2: Add Material Design and style main page.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { MdCoreModule, MdTableModule, MdToolbarModule } from '@angular/material';
+import { CdkTableModule } from '@angular/cdk';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IceCreamTableComponent } from './ice-cream-table/ice-cream-table.component';
+
+@NgModule({
+  exports: [
+    CdkTableModule,
+    MdCoreModule,
+    MdTableModule,
+    MdToolbarModule
+  ]
+})
+export class IceCreamMaterialModule { }
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IceCreamTableComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    IceCreamMaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
