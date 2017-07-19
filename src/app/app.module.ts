d57abf7459507d1aeda7684a740bb6f73b5a7e98
Michael Giambalvo <heathkit@google.com>
2017-07-18 17:53:17 -0700

    Step 8: Set up routes, add new flavor button.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,28 @@ import { CdkTableModule } from '@angular/cdk';
 import { NgModule } from '@angular/core';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { IceCreamTableComponent } from './ice-cream-table/ice-cream-table.component';
 import { FlavorService } from './flavor.service';
 import { NewFlavorComponent } from './new-flavor/new-flavor.component';
+
+export const appRoutes: Routes = [
+  {
+    path: 'new',
+    component: NewFlavorComponent,
+  },
+  {
+    path: 'flavors',
+    component: IceCreamTableComponent,
+  },
+  {
+    path: '',
+    redirectTo: '/flavors',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   exports: [
@@ -35,7 +52,8 @@ export class IceCreamMaterialModule { }
     BrowserAnimationsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
-    IceCreamMaterialModule
+    IceCreamMaterialModule,
+    RouterModule.forRoot(appRoutes, { useHash: true })
   ],
   providers: [FlavorService],
   bootstrap: [AppComponent]
